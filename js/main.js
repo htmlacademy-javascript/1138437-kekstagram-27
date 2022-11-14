326e3f37c4cf226d35d55bd801acb851a56064ce
Olesya Svezhinskaya <osvezhinskaya@gmail.com>
2022-11-14 17:41:14 +0300

    доделать

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+const { name } = require("browser-sync");
 
-function getRandom (min,max) {
+const PHOTO_COUNT = 25;
+
+const getRandom = (min,max) => {
   if (max < min) {
     return 0;
   } else (min < 0 || max < 0); {
@@ -7,15 +10,46 @@ function getRandom (min,max) {
   }
 
 return Math.floor(Math.random() * (max - min) ) + min;
+};
 
-}
+const getLength (str, length) {
+  return (str.length <= length);
+};
 
 
-function getLength (str,length) {
-count = str.length;
-if (count <= length) {
-  return true;
-}
-return false;
-}
+const ID = [
+1,
+2,
+3,
+4,
+];
+
+const URL = [
+'img/avatar-1.svg',
+'img/avatar-2.svg',
+'img/avatar-3.svg',
+'img/avatar-4.svg',
+];
+const DESCRIPTION =[
+
+];
+
+const createUser = () => {
+  return {
+    id: '',
+    avatar: '',
+    message: '',
+    name: ''
+  };
+};
+
+const addPhoto = (index) => ({
+  id: getRandom(1, 1000),
+  url: `/photos/${index}.jpg`,
+  description: '',
+  likes: getRandom(1, 100),
+  comments: Array.from({length: getRandom(0, 5)}, addComment)
+});
+
+const photos = Array.from({length: PHOTO_COUNT}, (_, index) => addPhoto(++index));
 
