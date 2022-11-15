d1f5efa0fd6a3c6b54fff4ab4a058513b456b53e
Olesya Svezhinskaya <osvezhinskaya@gmail.com>
2022-11-15 19:35:26 +0300

    Доделала

--- js/main.js
+++ js/main.js
@@ -2,6 +2,28 @@ const { name } = require("browser-sync");
 
 const PHOTO_COUNT = 25;
 
+let imageId = 1;
+
+let commentId = 1;
+
+const massages = [
+ 'Всё отлично!',
+ 'В целом всё неплохо. Но не всё.',
+ 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+ 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+ 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+ 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const names = [
+'Ария',
+'Георг',
+'Маша',
+'Сеня',
+'ДжиДжи',
+'Серго',
+];
+
 const getRandom = (min,max) => {
   if (max < min) {
     return 0;
@@ -12,42 +34,20 @@ const getRandom = (min,max) => {
 return Math.floor(Math.random() * (max - min) ) + min;
 };
 
-const getLength (str, length) {
-  return (str.length <= length);
-};
-
-
-const ID = [
-1,
-2,
-3,
-4,
-];
-
-const URL = [
-'img/avatar-1.svg',
-'img/avatar-2.svg',
-'img/avatar-3.svg',
-'img/avatar-4.svg',
-];
-const DESCRIPTION =[
-
-];
-
-const createUser = () => {
+const createComment = () => {
   return {
-    id: '',
-    avatar: '',
-    message: '',
-    name: ''
+    id:commentId++,
+    avatar: /img/${getRandom(1, 6)}.jpg,
+    message: messages[getRandom(0, 5)],
+    name: names[getRandom(0, 9)]
   };
 };
 
 const addPhoto = (index) => ({
-  id: getRandom(1, 1000),
+  id: imageId++,
   url: `/photos/${index}.jpg`,
-  description: '',
-  likes: getRandom(1, 100),
+  description: `фото №${imageId}`,
+  likes: getRandom(15, 200),
   comments: Array.from({length: getRandom(0, 5)}, addComment)
 });
 
